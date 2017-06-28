231bf00770235c17235693aa590d632e15565e22
Igor Šarić <igor.saric@outlook.com>
2017-06-28 20:34:46 +0200

    fixed plugin not loading on reopen

--- dev.js
+++ dev.js
@@ -24,81 +24,87 @@ function injectGitFileStatus()
                         const path = require("path");
                         const exec = require("child_process").exec;
 
-                        const gitRoot = path.normalize(path.dirname(explorerItem.getAttribute("title")));
-
                         const style = document.createElement("style");
                         document.body.appendChild(style);
 
-                        const startGitStatusChecks = () =>
+                        const resolveHome = (filepath) =>
                         {
-                            // run git status
-                            const command = "git status --short --ignored";
-
-                            const resolveHome = (filepath) =>
+                            if (filepath[0] === "~")
                             {
-                                if (filepath[0] === "~")
-                                {
-                                    return path.join(process.env.HOME, filepath.slice(1));
-                                }
-
-                                return filepath;
+                                return path.join(process.env.HOME, filepath.slice(1));
                             }
 
-                            const options =
-                            {
-                                cwd: resolveHome(gitRoot)
-                            };
-
-                            const callback = (error, stdout, stderr) =>
-                            {
-                                if (!error)
-                                {
-                                    const files = stdout.split("\n");
-
-                                    const normalize = (name) =>
-                                    {
-                                        return path.normalize(name.substr(3)).replace(/\\+$/, "").replace(/\/+$/, "").replace(/\\/g, "\\\\");
-                                    };
-
-                                    const added = files.filter(name => { return name.startsWith("?? "); }).map(name => { return normalize(name); });
-                                    const modified = files.filter((name) => { return name.startsWith(" M "); }).map(name => { return normalize(name); });
-                                    const deleted = files.filter((name) => { return name.startsWith("!! "); }).map(name => { return normalize(name); });
-
-                                    let html = "";
-                                    const classPath = "#workbench\\.view\\.explorer .explorer-folders-view .monaco-tree .monaco-tree-rows .monaco-tree-row .explorer-item";
-
-                                    added.forEach(addedFile =>
-                                    {
-                                        html += `${classPath}[title$="${addedFile}" i]{color:${addedColor};}`
-                                    });
-
-                                    modified.forEach(modifiedFile =>
-                                    {
-                                        html += `${classPath}[title$="${modifiedFile}" i]{color:${modifiedColor};}`
-                                    });
-
-                                    deleted.forEach(deletedFile =>
-                                    {
-                                        html += `${classPath}[title$="${deletedFile}" i]{opacity:${ignoredOpacity};}`
-                                    });
-
-                                    if (style.innerHTML !== html)
-                                    {
-                                        style.innerHTML = html;
-                                    }
-                                }
-
-                                setTimeout(startGitStatusChecks, timeout);
-                            }
-
-                            exec(command, options, callback);
+                            return filepath;
                         }
 
-                        startGitStatusChecks();
+                        const firstFileDir = path.normalize(path.dirname(explorerItem.getAttribute("title")));
+
+                        const gitRootCommand = "git rev-parse --show-toplevel";
+                        const gitRootOptions = { cwd: resolveHome(firstFileDir) };
+                        const gitRootCallback = (error, stdout, stderr) =>
+                        {
+                            if (!error)
+                            {
+                                const gitRoot = stdout.trim();
+                                const startGitStatusChecks = () =>
+                                {
+                                    // run git status
+                                    const gitStatusCommand = "git status --short --ignored";
+                                    const gitStatusOptions = { cwd: resolveHome(gitRoot) }
+                                    const gitStatusCallback = (error, stdout, stderr) =>
+                                    {
+                                        if (!error)
+                                        {
+                                            const files = stdout.split("\n");
+
+                                            const normalize = (name) =>
+                                            {
+                                                return path.normalize(name.substr(3)).replace(/\\+$/, "").replace(/\/+$/, "").replace(/\\/g, "\\\\");
+                                            };
+
+                                            const added = files.filter(name => { return name.startsWith("?? "); }).map(name => { return normalize(name); });
+                                            const modified = files.filter(name => { return name.startsWith(" M "); }).map(name => { return normalize(name); });
+                                            const deleted = files.filter(name => { return name.startsWith("!! "); }).map(name => { return normalize(name); });
+
+                                            let html = "";
+                                            const classPath = "#workbench\\.view\\.explorer .explorer-folders-view .monaco-tree .monaco-tree-rows .monaco-tree-row .explorer-item";
+
+                                            added.forEach(addedFile =>
+                                            {
+                                                html += `${classPath}[title$="${addedFile}" i]{color:${addedColor};}`
+                                            });
+
+                                            modified.forEach(modifiedFile =>
+                                            {
+                                                html += `${classPath}[title$="${modifiedFile}" i]{color:${modifiedColor};}`
+                                            });
+
+                                            deleted.forEach(deletedFile =>
+                                            {
+                                                html += `${classPath}[title$="${deletedFile}" i]{opacity:${ignoredOpacity};}`
+                                            });
+
+                                            if (style.innerHTML !== html)
+                                            {
+                                                style.innerHTML = html;
+                                            }
+                                        }
+
+                                        setTimeout(startGitStatusChecks, timeout);
+                                    }
+
+                                    exec(gitStatusCommand, gitStatusOptions, gitStatusCallback);
+                                }
+
+                                startGitStatusChecks();
+                            }
+                        }
+
+                        exec(gitRootCommand, gitRootOptions, gitRootCallback);
 
                         // loaded
                         return;
-                    }
+                    };
                 }
             }
         }
